e183a1c17bbba93fd1616ca0e6dfdea7fe382fc5
leslietan1981 <leslietan.yw1981@gmail.com>
2026-09-25 11:10:29 +0800

    Update formatUtils.ts

--- frontend/src/utils/formatUtils.ts
+++ frontend/src/utils/formatUtils.ts
@@ -18,3 +18,9 @@ export const toLocalISODate = (date: Date): string => {
 export const getTimeFromISODate = (ISODate: string): string => {
   return ISODate.split("T")[1].split(":").splice(0, 2).join(":");
 };
+
+export const toFixLength = (value: number, length: number): string => {
+  let result: string = String(value);
+  while (result.length < length) result = "0" + result;
+  return result;
+};
